0b7f6b1a6a22be2e2aab04999cadffd78a375ede
Ivan R <iriijo@googlemail.com>
2017-02-22 23:50:29 +0100

    Be less strict in If-Modified-Since

--- Bundler/BundlerHandler.cs
+++ Bundler/BundlerHandler.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Globalization;
-using System.Threading;
 using System.Web;
 using Bundler.Infrastructure;
 
 namespace Bundler {
     public sealed class BundlerHandler : IHttpHandler {
+        public const string IfModifiedSinceHeader = "If-Modified-Since";
+
         private readonly IBundle _bundle;
 
         public bool IsReusable { get; } = false;
@@ -16,9 +16,11 @@ namespace Bundler {
 
 
         public void ProcessRequest(HttpContext context) {
+            // TODO: Add cache enable setting.
+
             DateTime lastModification;
-            if (DateTime.TryParseExact(context.Request.Headers["If-Modified-Since"], "r", Thread.CurrentThread.CurrentCulture, DateTimeStyles.None , out lastModification) && lastModification > _bundle.LastModification) {
-                // 304
+            var lastModificationRaw = context.Request.Headers[IfModifiedSinceHeader];
+            if (!string.IsNullOrWhiteSpace(lastModificationRaw) && DateTime.TryParse(lastModificationRaw, out lastModification) && lastModification > _bundle.LastModification) {
                 context.Response.StatusCode = 304;
                 return;
             }
@@ -26,7 +28,8 @@ namespace Bundler {
             context.Response.Write(_bundle.Content);
             context.Response.ContentType = _bundle.ContentType;
             context.Response.StatusCode = 200;
-            
+
+            // TODO: Add cache enable setting.
             context.Response.Cache.SetLastModified(_bundle.LastModification);
             context.Response.Cache.SetExpires(DateTime.Now.AddYears(1));
         }
